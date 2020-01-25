8f4768cacc74b88418201573d0332de0e344a627
kirti <kirti.pitale02@gmail.com>
2020-01-25 08:45:01 +0530

    neoquant assignment uploaded on gitlab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assignment';
+export class AppComponent implements OnInit {
+  title = 'Users List';
+  allusers: any;
+  selectedarr: [];
+  msg: string;
+
+  constructor(private _http: HttpClient) { }
+  ngOnInit() {
+    this.userList();
+  }
+  userList() {
+    this.allusers = this._http.get('http://localhost:4200/assets/data/user.json')
+      .subscribe(data => this.allusers = data);
+  }
+
+  changeVehical(value: any | string, index: any) {
+    this.selectedarr = this.allusers[index].vehicles[value];
+    this.msg = `${this.allusers[index].name} selected ${value} as vehicle`;
+  }
 }
